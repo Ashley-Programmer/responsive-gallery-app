e1f75bb7ae5c4fd0116e2f6fe67cb0b357b35d31
Ashley <motsieashley31@gmail.com>
2025-05-18 04:55:12 +0200

    respinsive-gallery commit

---
+++ files/ReadME.txt
@@ -0,0 +1,2 @@
+for hamburger menu icon : &#9776;
+for close icon : &#10005;
---
+++ files/about.js
@@ -0,0 +1,8 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex'; // displays the side bar => flex
+}
+function closeSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none'; // closes/hides the side bar => none
+}
